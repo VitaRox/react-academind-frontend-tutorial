43c6324d7202c3b83c96049fc72ffddf0d3f028f
Vita <wiebetc@gmail.com>
2020-08-26 15:17:38 -0700

    - Adds in successful Google Maps API integration;

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import Input from '../../shared/components/FormElements/Input.js';
+
+import './NewPlace.css';
+
 function NewPlace() {
   return (
-    <h2>New place works</h2>
-  )
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text" label="Title"
+        validators={[]}
+        errorText="Please enter a valid title."
+      />
+    </form>
+  );
 }
 
 export default NewPlace;
