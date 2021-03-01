97080265e3b5a561788d83994cc884e4e40b7f83
Vita <vita.harvey@seattlecolleges.edu>
2021-02-28 16:23:04 -0800

    - Updates so does some stuff better;

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { useForm } from '../../shared/hooks/form-hook';
@@ -36,7 +36,7 @@ const DUMMY_PLACES = [
 ];
 
 const UpdatePlace = () => {
-
+  const [isLoading, setIsLoading] = useState(true);
   const placeId = useParams().placeId;
 
   const [formState, inputHandler, setFormData] = useForm({
@@ -69,13 +69,24 @@ const UpdatePlace = () => {
         value: identifiedPlace.description,
         isValid: true
       }
-    }, true);
-  }, [setFormData, identifiedPlace]);
+    },
+    true
+  );
+  setIsLoading(false);
+ }, [setFormData, identifiedPlace]);
 
   if (!identifiedPlace) {
     return (
       <div className="center">
         <h2>Could not find the place!</h2>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>L O A D I N G</h2>
       </div>
     );
   }
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -5,30 +5,30 @@ import PlaceList from '../../places/components/PlaceList';
 
 // Create dummy data easily for developing;
 const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empoire State Building',
-    description: 'Famousest skyscraper in New York City',
-    imageUrl: 'https://www.esbnyc.com/sites/default/files/styles/on_single_feature/public/2020-02/Green%20lights.jpg?itok=nRbtw3hG',
-    address: '20 W 34th St, New York, NY 10001',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878531
-    },
-    creator: 'u1'
-  },
-  {
-    id: 'p2',
-    title: 'Burj Khalifa',
-    description: 'A pretty goddamned big building, eh?',
-    imageUrl: 'https://cdn.britannica.com/37/135437-050-A12461CC/Burj-Khalifa-world-Sheikh-Khalifah-ibn-Zayid-2010.jpg',
-    address: '1 Sheikh Mohammed bin Rashid Blvd - Downtown Dubai - Dubai - United Arab Emirates',
-    location: {
-      lat: 25.197197,
-      lng: 55.2721877
-    },
-    creator: 'u2'
-  }
+  // {
+  //   id: 'p1',
+  //   title: 'Empoire State Building',
+  //   description: 'Famousest skyscraper in New York City',
+  //   imageUrl: 'https://www.esbnyc.com/sites/default/files/styles/on_single_feature/public/2020-02/Green%20lights.jpg?itok=nRbtw3hG',
+  //   address: '20 W 34th St, New York, NY 10001',
+  //   location: {
+  //     lat: 40.7484405,
+  //     lng: -73.9878531
+  //   },
+  //   creator: 'u1'
+  // },
+  // {
+  //   id: 'p2',
+  //   title: 'Burj Khalifa',
+  //   description: 'A pretty goddamned big building, eh?',
+  //   imageUrl: 'https://cdn.britannica.com/37/135437-050-A12461CC/Burj-Khalifa-world-Sheikh-Khalifah-ibn-Zayid-2010.jpg',
+  //   address: '1 Sheikh Mohammed bin Rashid Blvd - Downtown Dubai - Dubai - United Arab Emirates',
+  //   location: {
+  //     lat: 25.197197,
+  //     lng: 55.2721877
+  //   },
+  //   creator: 'u2'
+  // }
 ];
 
 
